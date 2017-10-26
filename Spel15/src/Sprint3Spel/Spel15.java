120b95b2c44b0e4c4c144b1656b89953d68a3991
Anna Wrang <anna.wrang@gmail.com>
2017-10-26 18:44:19 +0200

    la till metoder i Spel15

--- Spel15/src/Sprint3Spel/Spel15.java
+++ Spel15/src/Sprint3Spel/Spel15.java
@@ -1,8 +1,23 @@
 package Sprint3Spel;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 import javax.swing.*;
+
+/* 
+    Vi behöver 
+    1 ActionListener - anonym klass kanske?
+    2. sätta ut actionListeners för alla knappar
+    3. 
+
+*/
 
 public class Spel15 extends JFrame {
 
@@ -25,16 +40,84 @@ public class Spel15 extends JFrame {
     Button fourteen = new Button("14");
     Button fifteen = new Button("15");
     Button sixteen = new Button("16");
+    List<Button> buttonList = new ArrayList<>();
 
     public Spel15() {
-        this.setLayout(new GridLayout(2, 1));
-        this.add(game); this.add(panel); 
-
+        this.setLayout(new BorderLayout());
+        this.add(panel, BorderLayout.SOUTH);
+        this.add(game, BorderLayout.NORTH); 
         panel.add(newGame);
+     
+        game.setLayout(new GridLayout(4,4));
+        startNewGame();
 
         this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.pack();
+    }
+
+
+    public void startNewGame(){
+        buttonList = createButtonList();
+        Collections.shuffle(buttonList);
+        for(Button b: buttonList){
+            game.add(b);
+        }
+    }
+    
+
+    public List<Button> createButtonList() {
+        buttonList = new ArrayList<>();
+        buttonList.add(one);
+        one.setText("1");
+        one.setBackground(Color.CYAN);
+        buttonList.add(two);
+        two.setText("2");
+        two.setBackground(Color.CYAN);
+        buttonList.add(three);
+        three.setText("3");
+        three.setBackground(Color.CYAN);
+        buttonList.add(four);
+        four.setText("4");
+        four.setBackground(Color.CYAN);
+        buttonList.add(five);
+        five.setText("5");
+        five.setBackground(Color.CYAN);
+        buttonList.add(six);
+        six.setText("6");
+        six.setBackground(Color.CYAN);
+        buttonList.add(seven);
+        seven.setText("7");
+        seven.setBackground(Color.CYAN);
+        buttonList.add(eight);
+        eight.setText("8");
+        eight.setBackground(Color.CYAN);
+        buttonList.add(nine);
+        nine.setText("9");
+        nine.setBackground(Color.CYAN);
+        buttonList.add(ten);
+        ten.setText("10");
+        ten.setBackground(Color.CYAN);
+        buttonList.add(eleven);
+        eleven.setText("11");
+        eleven.setBackground(Color.CYAN);
+        buttonList.add(twelve);
+        twelve.setText("12");
+        twelve.setBackground(Color.CYAN);
+        buttonList.add(thirteen);
+        thirteen.setText("13");
+        thirteen.setBackground(Color.CYAN);
+        buttonList.add(fourteen);
+        fourteen.setText("14");
+        fourteen.setBackground(Color.CYAN);
+        buttonList.add(fifteen);
+        fifteen.setText("15");
+        fifteen.setBackground(Color.CYAN);
+        buttonList.add(sixteen);
+        sixteen.setText(" ");
+        sixteen.setBackground(Color.BLACK);
+
+        return buttonList;
     }
 
     public static void main(String[] args) {
