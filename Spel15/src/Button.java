e6716ff1766742585da07153469260021b240eb4
Anna Wrang <anna.wrang@gmail.com>
2017-10-26 15:42:03 +0200

    anna- second draft

---
+++ Spel15/src/Button.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ *  och öppna templates :)
+ */
+
+/**
+ *
+ * @author Anna
+ */
+public class Button {
+    
+}
