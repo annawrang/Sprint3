825ffc504217e9e66f8d984eca81b6fad7b0985a
Anna Wrang <anna.wrang@gmail.com>
2017-10-26 17:32:23 +0200

    anna- la till metod i button

--- Spel15/src/Sprint3Spel/Button.java
+++ Spel15/src/Sprint3Spel/Button.java
@@ -1,10 +1,20 @@
 package Sprint3Spel;
 
+import java.awt.Color;
 import javax.swing.*;
 
 public class Button extends JButton{
     
+    public Button(){}
+    
     public Button(String text){
         super(text);
     }
+    
+    // Metod man kan anropa som gör knappen man klickar på den nya tomma
+    public void newEmpty(){
+        this.setBackground(Color.BLACK);
+        this.setText(" ");
+    }
+    
 }
